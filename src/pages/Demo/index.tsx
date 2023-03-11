f2edc581f4f346fbba80a299ce7cfd1b51df61d7
Dwan-Ge <114384476+Dwan-Ge@users.noreply.github.com>
2023-03-11 16:44:07 +0800

    basic query and mutation

--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -1,19 +1,41 @@
-import { Paper } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
-import { getAllCourse } from "../../service/course";
+import { Button, Paper } from "@mui/material";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { COURSE_QUERY_KEY } from "../../react-query/courseQuery";
+import {
+  createCourse,
+  getAllCourse,
+  ICourseDetail,
+} from "../../service/course";
 
 export default () => {
+  const queryClient = useQueryClient();
   const courses = useQuery({
-    queryKey: ["getCourses"],
+    queryKey: [COURSE_QUERY_KEY],
     queryFn: getAllCourse,
   });
+  const addCourse = useMutation({
+    mutationFn: createCourse,
+    onSuccess: () => {
+      queryClient.invalidateQueries([COURSE_QUERY_KEY]);
+    },
+  });
+  const newCourse: ICourseDetail = {
+    title: "Hello, new Course",
+    description: "The Description",
+    duration: 60,
+    authors: ["9987de6a-b475-484a-b885-622b8fb88bda"],
+  };
 
   if (courses.isLoading) return <h1>Loading...</h1>;
+  if (courses.isError) return <h2>Error occured.</h2>;
   return (
     <div>
       {courses?.data?.map((item: any) => (
         <Paper key={item.id}>{item.title}</Paper>
       ))}
+      <Button variant="contained" onClick={() => addCourse.mutate(newCourse)}>
+        Add Course
+      </Button>
     </div>
   );
 };
--- src/service/course.ts
+++ src/service/course.ts
@@ -2,11 +2,32 @@ import axios from 'axios';
 
 const baseUrl = 'http://localhost:4000'
 
+export interface ICourseDetail {
+	id?: string;
+	title: string;
+	description: string;
+	duration: number;
+	authors: string[];
+}
+
 export const getAllCourse = () => axios.get(`${baseUrl}/courses/all`)
 .then((res) => {
-	console.log('res: ', res.data.result)
 	return res.data.result
 })
+.catch((reason) => {
+	console.log(reason)
+})
+
+export const createCourse = (course: ICourseDetail) => axios.post(
+	`${baseUrl}/courses/add`, 
+	course, 
+	{
+		headers: {
+		'Content-Type': 'application/json;charset=utf-8',
+		'Cache-Control': 'no-cache',
+		Authorization: 'Bearer FNJ0pHl7mCuFhlgkXECGw2X1F40+X9LNFspuOicKYvI/PZCbUeKM1GpB7Z8WtobO3ZloyCrwKt2kuRkQaApDWgB71WUxnHovf3z2hEwx5bSI//6erzr3cRAM9UjpIAEfqiZoPDde+wMFovMwbNBDYqTJrgkHTfahcXOgCAukTByqLLGs2pmTNvmEdifd4OukgszIVMup6/NPXivjcR0IYAYuKYAqiLpXrZ73IpNmIIrLwX8OJPgEZafC9c8TTFoQVWuZ42kGYasvMGbjo2sUhqSJtzjHF+P3Xz9w1eLup3/MBHRu5JxCF2QarJPQJfsaZZQgHZ2nkSa2p8DU+4Px1w=='
+	}
+}).then((res) => res.data.result)
 .catch((reason) => {
 	console.log(reason)
 })
